7cfc49669a06acf56b8fbc442d6b3b55b293a180
Reyhaneh <reyhaneh179@yahoo.com>
2021-11-12 13:16:01 +0330

    First commit

---
+++ src/test/java/com/reyrey/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reyrey.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

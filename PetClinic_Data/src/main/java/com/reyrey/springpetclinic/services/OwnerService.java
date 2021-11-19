a65663baed9144e79489204a524178f637577801
Reyhaneh <reyhaneh179@yahoo.com>
2021-11-19 20:24:27 +0330

    Impelement interfaces for pet clinic services.

---
+++ PetClinic_Data/src/main/java/com/reyrey/springpetclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package com.reyrey.springpetclinic.services;
+
+import com.reyrey.springpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}

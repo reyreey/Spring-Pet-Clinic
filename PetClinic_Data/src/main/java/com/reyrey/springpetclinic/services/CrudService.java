b2a539598add725fdc9cc79e8496d2f96b105175
Reyhaneh <reyhaneh179@yahoo.com>
2021-11-19 22:09:29 +0330

    implement map based services.

--- PetClinic_Data/src/main/java/com/reyrey/springpetclinic/services/CrudService.java
+++ PetClinic_Data/src/main/java/com/reyrey/springpetclinic/services/CrudService.java
@@ -4,9 +4,9 @@ import java.util.Set;
 
 public interface CrudService<T,ID> {
     Set<T> findAll();
-    T findByID(ID id);
+    T findById(ID id);
     T save(T object);
     void delete(T object);
-    void deleteByID(ID id);
+    void deleteById(ID id);
 
 }

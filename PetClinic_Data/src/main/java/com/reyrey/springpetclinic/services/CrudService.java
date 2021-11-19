85688c1cf1b21f5125c1f80fed310915aa8c8913
Reyhaneh <reyhaneh179@yahoo.com>
2021-11-19 20:55:49 +0330

    adding CrudService interface.

---
+++ PetClinic_Data/src/main/java/com/reyrey/springpetclinic/services/CrudService.java
@@ -0,0 +1,12 @@
+package com.reyrey.springpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T,ID> {
+    Set<T> findAll();
+    T findByID(ID id);
+    T save(T object);
+    void delete(T object);
+    void deleteByID(ID id);
+
+}
--- PetClinic_Data/src/main/java/com/reyrey/springpetclinic/services/PetService.java
+++ PetClinic_Data/src/main/java/com/reyrey/springpetclinic/services/PetService.java
@@ -4,8 +4,5 @@ import com.reyrey.springpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet,Long> {
 }

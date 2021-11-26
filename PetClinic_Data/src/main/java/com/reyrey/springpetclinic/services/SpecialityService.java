f4616f82be5d9106fa0b857abf27a252c4a2f314
Reyhaneh <reyhaneh179@yahoo.com>
2021-11-26 19:57:23 +0330

    added speciality Map impl.

---
+++ PetClinic_Data/src/main/java/com/reyrey/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.reyrey.springpetclinic.services;
+
+import com.reyrey.springpetclinic.model.Speciality;
+import com.reyrey.springpetclinic.services.CrudService;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}

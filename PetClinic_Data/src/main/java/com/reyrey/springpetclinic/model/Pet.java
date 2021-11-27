b97eaafb08de05eae5d3744ec44811a63f1ac1c4
Reyhaneh <reyhaneh179@yahoo.com>
2021-11-27 22:35:42 +0330

    Added visit entity.

--- PetClinic_Data/src/main/java/com/reyrey/springpetclinic/model/Pet.java
+++ PetClinic_Data/src/main/java/com/reyrey/springpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.reyrey.springpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pet")
@@ -16,6 +18,8 @@ public class Pet extends BaseEntity{
     private Owner owner;
     @Column(name = "birth_date")
     private LocalDate birthDate;
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits=new HashSet<>();
 
     public String getName() {
         return name;
@@ -47,5 +51,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
